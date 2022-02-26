5cc836deb59b461be9de36f2e7bac93835bd5c9a
Luis Ricardo Benhossi <ricardo.benhossi@gmail.com>
2022-02-26 19:41:01 -0300

    Static methods and private methods.

--- conta.py
+++ conta.py
@@ -11,16 +11,27 @@ class Conta:
         self.__saldo = dec.Decimal(saldo)   # PYTHON DOESN´T FORBID YOU, BUT WARN YOU THAT YOU SHOLD NOT DO IT
         self.__limite = limite              # BY SHOWING UNDERSCORES WHEN YOU CALL THESE ATTRIBUTES
 
+    def __permite_saque(self, valor_saque):
+        disponivel_para_saque = self.__saldo + self.__limite
+        return valor_saque <= disponivel_para_saque
+
     def extrato(self):
         print(f"O saldo da conta do titular {self.__titular} é de R${self.__saldo}.")
 
-    def deposita(self, valor):
-        self.__saldo += dec.Decimal(valor)
-        print(f"Depósito de R${valor} realizado com sucesso.")
+    def deposita(self, valor_deposito):
+        if (valor_deposito > 0):
+            self.__saldo += dec.Decimal(valor_deposito)
+            print(f"Depósito de R${valor_deposito} realizado com sucesso.")
+        else:
+            print(f"Valor de R${valor_deposito} informado é inválido.")
+            print("Informe um valor maior que 0.")
 
     def saca(self, valor):
-        self.__saldo -= dec.Decimal(valor)
-        print(f"Saque de R${valor} realizado com sucesso.")
+        if (self.__permite_saque(valor)):
+            self.__saldo -= dec.Decimal(valor)
+            print(f"Saque de R${valor} realizado com sucesso.")
+        else:
+            print("Saldo sinsuficiente para o saque.")
 
     def transefere(self, valor: float, conta_destino: "Conta"):
         self.saca(dec.Decimal(valor))
@@ -46,3 +57,11 @@ class Conta:
     @limite.setter
     def limite(self, novo_limite):
         self.__limite = novo_limite
+
+    @staticmethod
+    def banco():
+        return "341"
+
+    @staticmethod
+    def bancos():
+        return {"BB": "001", "ITAU": "341", "NuBank": "260"}
